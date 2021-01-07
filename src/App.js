08a2de184ab3ac721bab9a45fa32b3d02d7abba9
syanenko65 <syanenko65@gmail.com>
2021-01-07 12:54:24 +0300

    Drawer

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { Slider } from '@material-ui/core';
 import { Checkbox } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { FormGroup } from '@material-ui/core';
 import { FormControlLabel } from '@material-ui/core';
 import { ListSubheader } from '@material-ui/core';
@@ -27,13 +27,21 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 import { MovieFilter,
          PanTool,
          ExpandMore,
          ExpandLess } from '@material-ui/icons';
 
+/*
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
@@ -52,7 +60,66 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
 }));
+*/
 
+const drawerWidth = 340;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  },
+}));
 
 //
 // Debug marks checkbox
@@ -411,42 +478,121 @@ function EffectsList() {
 }
 
 //
-// Application bar
+// Drawer
 //
-function ButtonAppBar() {
+export default function PersistentDrawerRight() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" noWrap className={classes.title}>
             Shader play
           </Typography>
-          <Button color="inherit">About</Button>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            className={clsx(open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <div className={classes.root}>
+          <Grid container spacing={2}>
+            <Grid item id="scene" xs={6}/><Grid item xs={6}/>
+            <Grid item id="info" xs={6} /><Grid item xs={6}/>
+          </Grid>
+        </div>
+      </main>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['', '', '', ''].map((text, index) => (
+            <ListItem button key={text} >
+              <Grid container spacing={2}>
+                <Grid item id={'slot_' + index} xs/>
+              </Grid>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 }
 
 //
+// TODO: Drawer -> List(Hands/Filters) -> Accordeon -> List (Effect name/Icon/Controls)
+//
+
+//
 // Rendering 
 //
+/*
 ReactDOM.render(
   <Layout />,
   document.getElementById("root")
 );
+*/
 
 ReactDOM.render(
-  <ButtonAppBar />,
-  document.getElementById("app_bar")
+  <PersistentDrawerRight />,
+  document.getElementById("root")
 );
 
+/*
 ReactDOM.render(
   <EffectsList />,
   document.getElementById("effects")
 );
+*/
 
 ReactDOM.render(
   <Scene />,
