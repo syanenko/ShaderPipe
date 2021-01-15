5940e1940e4b58aa23907f0734afdffef11c38a9
syanenko65 <syanenko65@gmail.com>
2021-01-15 17:11:31 +0300

    Effects icon changed

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ import { Divider } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import clsx from 'clsx';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -361,7 +360,7 @@ export default function PersistentDrawerRight() {
             edge="end"
             onClick={handleEffectsOpen}
             className={clsx(open && classes.hide)}>
-            <MenuIcon />
+            <BubbleChart />
           </IconButton>
           
         </Toolbar>
