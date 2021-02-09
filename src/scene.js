220e163f450d4aa8128e2df33f7bbd9dd5577995
syanenko65 <syanenko65@gmail.com>
2021-02-09 04:58:27 +0300

    Geometry mask

--- src/scene.js
+++ src/scene.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as THREE from 'three';
 import { resolution, materials } from './materials';
+import { face }      from './mediapipe';
 import { activeMat } from './App';
 
 //
@@ -8,13 +9,14 @@ import { activeMat } from './App';
 //
 var mesh;
 var renderer;
+var mask;
   
 class Scene extends React.Component
 {
   componentDidMount()
   {
     const scene    = new THREE.Scene();
-    const camera   = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+    const camera   = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(resolution.x, resolution.y);
 
@@ -24,7 +26,11 @@ class Scene extends React.Component
 
     const geometry = new THREE.PlaneBufferGeometry( 2, 2 );
     mesh = new THREE.Mesh(geometry, materials[activeMat]);
+    mesh.translateZ(-0.1); 
     scene.add(mesh);
+
+    var maskMat = new THREE.LineBasicMaterial( { color: 0x00ff00 } );
+    var maskGeom, mask;
 
     var animate = function ()
     {
@@ -33,7 +39,20 @@ class Scene extends React.Component
       if(materials[activeMat].uniforms['u_time'])
         materials[activeMat].uniforms['u_time'].value = performance.now() / 1000;
 
+      if(materials[activeMat].uniforms['u_face'])
+      {
+        maskGeom = new THREE.BufferGeometry().setFromPoints( face );
+        mask = new THREE.Line( maskGeom, maskMat );
+        scene.add(mask);
+      }
+    
       renderer.render(scene, camera);
+
+      if(materials[activeMat].uniforms['u_face'])
+      {
+        scene.remove(mask);        
+        maskGeom.dispose();
+      }
     };
 
     animate();
